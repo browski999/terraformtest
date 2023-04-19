51773083e65fa4ae6735c803524b3e6db346d6a5
Robert Wright <robert.wright@emeal.nttdata.com>
2023-04-19 14:39:59 +0100

    new storage

--- main.tf
+++ main.tf
@@ -1,20 +1,46 @@
-# Configure the Azure provider
 terraform {
   required_providers {
     azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.0.2"
+        source = "hashicorp/azurerm"
+        version = "3.0.0"
     }
   }
-
-  required_version = ">= 1.0.4"
+  backend "azurerm" {
+    resource_group_name  = "tfdemo-rg"
+    storage_account_name = "tfstatebob"
+    container_name       = "tfstatecontainer"
+    key                  = "terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
-  features {}
+  features {}  
+}
+
+variable "location" {
+  type = string
+  default = "UK South"
 }
 
 resource "azurerm_resource_group" "rg" {
-  name     = "myTFResourceGroup"
-  location = "UK South"
+  name = "tfdemo-rg"
+  location = var.location
+}
+
+resource "azurerm_storage_account" "tfstate" {
+  name                     = "tfstatebob"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"  
+}
+
+resource "azurerm_storage_container" "tfstatecontainer" {
+  name                  = "tfstatecontainer"
+  storage_account_name  = azurerm_storage_account.tfstate.name
+  container_access_type = "private"
+}
+
+output "sa_name" {
+  value = azurerm_storage_account.tfstate.name
 }
