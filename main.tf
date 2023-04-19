d67244975dc4b6f8b0185f615d6cf5434daaf0ac
Robert Wright <robert.wright@emeal.nttdata.com>
2023-04-19 14:34:11 +0100

    start again

--- main.tf
+++ main.tf
@@ -6,12 +6,7 @@ terraform {
       version = "~> 3.0.2"
     }
   }
-#  backend "azurerm" {
-#    resource_group_name  = "myTFResourceGroup"
-#    storage_account_name = "tfstatebob"
-#    container_name       = "tfstatecontainer"
-#    key                  = "terraform.tfstate"
-#  }
+
   required_version = ">= 1.0.4"
 }
 
@@ -19,30 +14,7 @@ provider "azurerm" {
   features {}
 }
 
-variable "location" {
-  type = string
-  default = "UK South"
-}
-
 resource "azurerm_resource_group" "rg" {
   name     = "myTFResourceGroup"
-  location = var.location
+  location = "UK South"
 }
-
-#resource "azurerm_storage_account" "tfstate" {
-#  name                     = "tfstatebob"
-#  resource_group_name      = azurerm_resource_group.rg.name
-#  location                 = azurerm_resource_group.rg.location
-#  account_tier             = "Standard"
-#  account_replication_type = "LRS"  
-#}
-
-#resource "azurerm_storage_container" "tfstatecontainer" {
-#  name                  = "tfstatecontainer"
-#  storage_account_name  = azurerm_storage_account.tfstate.name
-#  container_access_type = "private"
-#}
-
-/* output "sa_name" {
-  value = azurerm_storage_account.tfstate.name
-} */
