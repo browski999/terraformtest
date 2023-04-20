36934367f8e621998ad2470bd677450ab98b8f43
Robert Wright <robert.wright@emeal.nttdata.com>
2023-04-20 10:50:39 +0100

    back to basic test

--- main.tf
+++ main.tf
@@ -1,46 +1,20 @@
+# Configure the Azure provider
 terraform {
   required_providers {
     azurerm = {
-        source = "hashicorp/azurerm"
-        version = "3.0.0"
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
     }
   }
-  /* backend "azurerm" {
-    resource_group_name  = "myTFResourceGroup"
-    storage_account_name = "tfstatebob"
-    container_name       = "tfstatecontainer"
-    key                  = "terraform.tfstate"
-  } */
+
+  required_version = ">= 1.0.4"
 }
 
 provider "azurerm" {
-  features {}  
-}
-
-variable "location" {
-  type = string
-  default = "UK South"
+  features {}
 }
 
 resource "azurerm_resource_group" "rg" {
-  name = "myTFResourceGroup"
-  location = var.location
-}
-
-resource "azurerm_storage_account" "tfstate" {
-  name                     = "tfstatebob"
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"  
-}
-
-resource "azurerm_storage_container" "tfstatecontainer" {
-  name                  = "tfstatecontainer"
-  storage_account_name  = azurerm_storage_account.tfstate.name
-  container_access_type = "blob"
-}
-
-output "sa_name" {
-  value = azurerm_storage_account.tfstate.name
+  name     = "myTFResourceGroup"
+  location = "UK South"
 }
